66f66f57718ac79b711ca138e3d2a9d95b450d48
Kevin Brown <kevin.brown@coina.ge>
2018-08-10 14:02:17 +1000

    Fix for data corruption bug.

--- contracts/Caller.sol
+++ contracts/Caller.sol
@@ -3,6 +3,6 @@ pragma solidity 0.4.24;
 contract Caller {
     function call(address receiverAddress) public {
         bytes memory test = "0x12345678";
-        receiverAddress.call(abi.encodeWithSignature("tokenFallback(address,uint256,bytes)"), msg.sender, 100, test);
+        receiverAddress.call(abi.encodeWithSignature("tokenFallback(address,uint256,bytes)", msg.sender, 100, test));
     }
 }
