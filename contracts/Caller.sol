d729da4b445d195be4a480a996ddccd9100ac189
Kevin Brown <kevin.brown@coina.ge>
2018-08-13 10:18:38 +1000

    Committing fix and comment on reproduction in case others stumble on this in the future.

--- contracts/Caller.sol
+++ contracts/Caller.sol
@@ -3,6 +3,13 @@ pragma solidity 0.4.24;
 contract Caller {
     function call(address receiverAddress) public {
         bytes memory test = "0x12345678";
+
+        // NOTE: Fixed in Solidity 0.5
+        // This syntax will corrupt the parameters of the function when doing the call and will no longer be allowed in Solidity 0.5:
+        //
+        // receiverAddress.call(abi.encodeWithSignature("tokenFallback(address,uint256,bytes)"), msg.sender, 100, test);
+        //
+        // While this one works fine.
         receiverAddress.call(abi.encodeWithSignature("tokenFallback(address,uint256,bytes)", msg.sender, 100, test));
     }
 }
